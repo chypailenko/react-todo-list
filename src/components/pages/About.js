467f0ad1187046dfc1904de497e63c77f917218e
polin <chypailenko@gmail.com>
2019-01-11 07:38:24 +0200

    added router

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+function About() {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aperiam atque commodi, cum delectus deserunt, dolor dolorem dolorum eius enim ex itaque labore laborum libero, nobis repellat temporibus unde veritatis!
+        </React.Fragment>
+    )
+}
+
+export default About;
